6a0702e2410dadcfe05738180b6ab581a24f4aa7
Ya'ar Hever <yaarhever@gmail.com>
2018-04-15 22:39:09 +0200

    Bootstrapped everything

---
+++ webpack.config.ts
@@ -0,0 +1,41 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
+
+
+const DIST = path.resolve(__dirname, 'dist');
+
+const config: webpack.Configuration = {
+  entry: [
+    './src/index.tsx'
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: DIST,
+    publicPath: '/'
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: DIST
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'tetris',
+      template: 'index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  }
+};
+
+export default config;
